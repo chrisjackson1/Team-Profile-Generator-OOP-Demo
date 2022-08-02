a960adffa4f108260e1aba832f32ca987144165c
Chris Jackson <chrisjackson@Chriss-MBP.fios-router.home>
2022-08-02 13:15:11 -0400

    still adding changes and updating for this demo

--- _tests_/Manager.test.js
+++ _tests_/Manager.test.js
@@ -1,17 +1,16 @@
 const Manager = require("../lib/Manager");
 const Employee = require("../lib/Employee");
 
-// test constructor to set office number
+
 test("", () => {
   
 });
 
-// test get role method. it should return "Manager"
+
 test("", () => {
 
 });
 
-// test get office number method
 test("", () => {
 
 });
